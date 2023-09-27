9e3036984fa3654b770ad72d4330121607e23274
Wu Wang Yang <56248896+wuwangyang24@users.noreply.github.com>
2023-09-27 18:09:58 +0200

    Update Customized_TOD.py

--- Customized_TOD.py
+++ Customized_TOD.py
@@ -1,18 +1,24 @@
+import torch.nn as nn
+
 class TOD(nn.Module):
     """
     Customized TOD Bert 
     """
     def __init__(self, root_model, num_classes):
-        super(CustomModel, self).__init__()
+        super(TOD, self).__init__()
         self.root_model = root_model
-        self.classifier = nn.Linear(root_model.config.hidden_size, num_classes, bias=True)
-        self.softmax = nn.Softmax()
+        self.classifier = nn.Linear(root_model.config.hidden_size, 1, bias=True)
+        self.softmax = nn.Softmax(dim=-1)
         self.loss = nn.CrossEntropyLoss()
     
     def forward(self, input_ids, attention_mask, token_type_ids, labels):
+        dim0, dim1 = input_ids.shape[0], input_ids.shape[1]
+        input_ids = input_ids.view(dim0*dim1, -1)
+        attention_mask = attention_mask.view(dim0*dim1, -1)
+        token_type_ids = token_type_ids.view(dim0*dim1, -1)
         x = self.root_model(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
-        x = self.classifier(x)
-        x = x.squeeze(-1)
+        x = self.classifier(x[1])
+        x = x.view(dim0,-1)
         x = self.softmax(x)
-        l = loss(x, labels)
+        l = self.loss(x, labels)
         return {"loss":l, "logits":x}
